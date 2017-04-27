dde237f9cf4ef487ab1473641304e3c14d261987
Pratip Rana <ranap@vcu.edu>
2017-04-27 00:22:44 -0400

    gateway 10 new

--- LAFO_gateway_prediction/value_LFAO.m
+++ LAFO_gateway_prediction/value_LFAO.m
@@ -2,9 +2,9 @@
 % Added Fragmentation
 function value_LFAO
 
-n=23; %gateway 9
+n=24; %gateway 10
 
-x=127e-3;
+x=137e-3;
 % x=70e-3;  %10um
 x1 =30e-3;
 y=20e3;
@@ -39,7 +39,7 @@ for i=2:13
 signalON=signalON + Y_val(:,i)*i;
 end
 
-signalON=signalON + Y_val(:,14)*24+ + Y_val(:,15)*36 + Y_val(:,16)*48+ Y_val(:,17)*60 + Y_val(:,18)*72+ Y_val(:,19)*84 + Y_val(:,20)*96 + Y_val(:,21)*108 +Y_val(:,n-1)*80000;
+signalON=signalON + Y_val(:,14)*24+ + Y_val(:,15)*36 + Y_val(:,16)*48+ Y_val(:,17)*60 + Y_val(:,18)*72+ Y_val(:,19)*84 + Y_val(:,20)*96 + Y_val(:,21)*108 + Y_val(:,22)*120 +Y_val(:,n-1)*80000;
 
 signalON(end)
  
