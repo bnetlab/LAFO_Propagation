a98a11ff4a8cd00800282d059eb15c5fc3c0d792
Pratip Rana <ranap@vcu.edu>
2017-04-30 18:35:42 -0400

    gateway 3

--- Lateral_association_moadel_gateway_prediction/value_LFAO.m
+++ Lateral_association_moadel_gateway_prediction/value_LFAO.m
@@ -2,9 +2,9 @@
 % Added Fragmentation
 function value_LFAO
 
-n=16; %gateway 2
+n=17; %gateway 3
 
-x=62e-3;
+x=72e-3;
 % x=70e-3;  %10um
 x1 =15e-3;
 y=15e3;
@@ -43,7 +43,7 @@ for i=14:n-2
 signalON=signalON + 12*(i-12);
 end
 
-signalON=signalON +Y_val(:,n-1)*250000;
+signalON=signalON +Y_val(:,n-1)*200000;
 
 signalON(end)
  
