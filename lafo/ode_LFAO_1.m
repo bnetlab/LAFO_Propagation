b097b521e0f6def635050312c6f2305de1e4bc01
Pratip Rana <ranap@vcu.edu>
2017-05-03 00:51:07 -0400

    global fit

--- lafo/ode_LFAO_1.m
+++ lafo/ode_LFAO_1.m
@@ -34,7 +34,7 @@ end
 
 Jfag=kfag * A(13) - kfag_ * A(1).^2;
 Jfb1=kfb1 * A(n-2)*A(n)- kfb1_ * A(n-1); %
-Jfb2= kfb2 *A(n-1) *A(n).^0.4-kfb2_ * A(n-1); %
+Jfb2= kfb2 *A(n-1) *A(n)-kfb2_ * A(n-1); %
 
 Jfb3=kfb3 * A(n-2)*A(12)- kfb3_ * A(n-1); %
 
@@ -52,56 +52,4 @@ end
 dA_dt(n-2)= Jla(n-3)-Jfb1-Jfb3; %
 dA_dt(n-1)=Jfb1+Jfb3; %
 dA_dt(n)=-sum(Jnu)-Jfb1-Jfb2;
-function dA_dt=ode_LFAO_1(t,A ,n,theta)
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
-% Added Fragmentation
-% Defines the set of ODEs to be solved to simulate insulin fibrillation %
-% A(1~n-1) is the vector of i-mer concentrations (i=1~n-1) %
-% A(n) is fibril conc. and A(n+1) is natural hexamer conc. %
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
-dA_dt=zeros(size(A)); % First order derivatives of i-mer concentrations
-% Flux of i-th fibrillation reaction
-Jla=zeros(1,n);
-
-knu=theta(1);
-knu_=theta(2);
-kla=theta(3); % First forward fibrillation rate constant
-kla_=theta(4);
-kfb1=theta(5);
-kfb1_=theta(6);
-kfb2=theta(7);
-kfb2_=theta(8);
-kfag=theta(9);
-kfag_=theta(10);
-kfb3=theta(11);
-kfb3_=theta(12);
-
-
-% Definitions of reaction fluxes Jfb
-for i=1:11
- Jnu(i)=knu*A(n)*A(i)-knu_*A(i+1); % The flux of i-mer nucleation rxn
 end
-for i=12:14
- Jla(i)=kla*A(12)*A(i)-kla_*A(i+1); % The flux of i-mer nucleation rxn
-end
-
-Jfag=kfag * A(12) - kfag_ * A(1).^2;
-Jfb1=kfb1 * A(15)*A(n)- kfb1_ * A(16);
-Jfb2= kfb2 *A(16) *A(n)-kfb2_ * A(16);
-
-Jfb3=kfb3 * A(15)*A(12)- kfb3_ * A(16);
-
-
-% There are n equations representing the conc. change of n species
-
-dA_dt(1)=-Jnu(1)+2 * Jfag; % Derivative of monomer conc.
-for i=2:11 % from dimer to (n-1)-mer
- dA_dt(i)=-Jnu(i)+Jnu(i-1); % Derivatives of oligomer concentrations
-end
-dA_dt(12)=Jnu(11)-sum(Jla(12:14))-Jla(12)-Jfag-Jfb3;
-for i=13:14 % from dimer to (n-1)-mer
- dA_dt(i)=-Jla(i)+Jla(i-1); % Derivatives of oligomer concentrations
-end
-dA_dt(15)= Jla(14)-Jfb1;
-dA_dt(16)=Jfb1+Jfb3;
-dA_dt(n)=-sum(Jnu)-Jfb1-Jfb2;
--- lafo/value_LFAO.m
+++ lafo/value_LFAO.m
@@ -4,22 +4,22 @@ function value_LFAO
 
 n=18; %gateway 10
 
-x=76e-3;
+x=38e-3;
 % x=70e-3;  %10um
 x1 =10e-3;
-y=20e3;
+y=5e6;
 y1=5e-2; 
-z=5e3;
+z=5e5;
 z1=5e-3;
-p=5e3;
-p1=10e-3; 
-q=100e-3;
+p=5e5;
+p1=5e-3; 
+q=40e-3;
 q1=0;
 s=0;
 s1=0;
 
 A_1=0.5;
-A_12=0.10;
+A_12=0.1;
 
 theta=[x,x1,y,y1,z,z1,p,p1,q,q1,s,s1]; 
 Y0=zeros(1,n); 
@@ -39,7 +39,7 @@ for i=2:13
 signalON=signalON + Y_val(:,i)*(i-1);
 end
 
-signalON=signalON + Y_val(:,14)*24+ + Y_val(:,15)*36 + Y_val(:,16)*48+Y_val(:,n-1)*200000;
+signalON=signalON + Y_val(:,14)*24+ + Y_val(:,15)*36 + Y_val(:,16)*48+Y_val(:,n-1)*3500000;
 
 signalON(end)
  
@@ -47,8 +47,8 @@ signalON = (signalON - min(signalON))/(max(signalON) - min(signalON));
 
 plot(t_range, signalON)
 hold on;
-load 'LFAO_DATA.txt';
-Data=LFAO_DATA;
+load 'LFAO_DATA_01.txt';
+Data=LFAO_DATA_01;
 plot(Data(:,1),Data(:,2),'-*')
 
 X=Data(:,2);
