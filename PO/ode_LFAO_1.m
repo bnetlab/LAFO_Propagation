729bc5f15aed3c9708621904d8732d67e9486e68
Pratip Rana <ranap@vcu.edu>
2017-05-16 21:14:19 -0400

    gateway 4

---
+++ PO/ode_LFAO_1.m
@@ -0,0 +1,68 @@
+function dA_dt=ode_LFAO_1(t,A ,n,theta)
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+
+% Added Fragmentation
+% Defines the set of ODEs to be solved to simulate insulin fibrillation %
+% A(1~n-1) is the vector of i-mer concentrations (i=1~n-1) %
+% A(n) is fibril conc. and A(n+1) is natural hexamer conc. %
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+dA_dt=zeros(size(A)); % First order derivatives of i-mer concentrations
+% Flux of i-th fibrillation reaction
+Jla=zeros(1,n);
+Jfb3=zeros(1,n);
+
+knu=theta(1);
+knu_=theta(2);
+kla=theta(3); % First forward fibrillation rate constant
+kla_=theta(4);
+kfb1=theta(5);
+kfb1_=theta(6);
+kfb3=theta(7);
+kfb3_=theta(8);
+kfag=theta(9);
+kfag_=theta(10);
+kfb2=theta(11);
+kfb2_=theta(12);
+kfb4=theta(13);
+kfb4_=theta(14);
+
+% Definitions of reaction fluxes Jfb
+for i=1:12
+ Jnu(i)=knu*A(n)*A(i)-knu_*A(i+1); % The flux of i-mer nucleation rxn
+end
+for i=13:n-15 %
+ Jla(i)=kla*A(13)*A(i)-kla_*A(i+1); % The flux of i-mer nucleation rxn
+end
+
+% Jfag=kfag1 * A(13) - kfag1_ * A(1).^2;
+Jfag=0;
+Jfb1=kfb1 * A(n-14)*A(n-14)- kfb1_ * A(n-13); %
+Jfb2= kfb2 *A(n-14) *A(n-13)-kfb2_ * A(n-13);
+Jfb4a= kfb4 *A(13) *A(n-14)-kfb4_ * A(n-13);%
+Jfb4b= kfb4/10 *A(13) *A(n-13)-kfb4_ * A(n-13);
+
+for i=n-13:n-2
+ Jfb3(i)=kfb3*A(i)*A(n)-kfb3_*A(i+1); % The flux of i-mer nucleation rxn
+end
+
+Jfb4=kfag*A(n-1);
+
+% There are n equations representing the conc. change of n species
+
+dA_dt(1)=-Jnu(1)+2*Jfag+Jfb4; % Derivative of monomer conc.
+for i=2:12 % from dimer to (n-1)-mer
+ dA_dt(i)=-Jnu(i)+Jnu(i-1); % Derivatives of oligomer concentrations
+end
+dA_dt(13)=Jnu(12)-sum(Jla)-Jla(13)-Jfag-Jfb4a-Jfb4b; %
+for i=14:n-15 % from dimer to (n-1)-mer %
+ dA_dt(i)=-Jla(i)+Jla(i-1); % Derivatives of oligomer concentrations
+end
+dA_dt(n-14)= Jla(n-15)-2*Jfb1-Jfb2-Jfb4a;
+dA_dt(n-13)=Jfb1-Jfb3(n-13)+Jfb4+Jfb4a;
+
+for i=n-12:n-2% from dimer to (n-1)-mer
+ dA_dt(i)=-Jfb3(i)+Jfb3(i-1); % Derivatives of oligomer concentrations
+end
+dA_dt(n-1)=Jfb3(n-2)-Jfb4;
+dA_dt(n)=-sum(Jnu)-sum(Jfb3);
+end
---
+++ PO/value_LFAO.m
@@ -0,0 +1,122 @@
+%looping for 3D plot
+% Added Fragmentation
+function value_LFAO
+
+n=30; %24 mer frag
+
+x=57e-3; %nu
+x1 =8e-3;
+y=2e5; %ilafo
+y1=5e-3; 
+z=5e5; %plafo
+z1=5e-2;
+p=5e5; %fib
+p1=5e-1; 
+q=5e2; % fag
+q1=0;
+r=1e5;
+r1=5e-2;
+s=1e5;
+s1=5e-2;
+
+A_1=0.5;
+B_12=[0.1 0.01 0.001];
+
+for j=1:3
+    A_12=B_12(j);
+
+theta=[x,x1,y,y1,z,z1,p,p1,q,q1,r,r1,s,s1]; 
+Y0=zeros(1,n); 
+
+Y0(n)=A_1;
+Y0(1)=A_12;
+t_range=linspace(0,337,337); 
+[t_val,Y_val]=ode23s(@ode_LFAO_1,t_range,Y0,[],n,theta);
+Y_val([1:20:300 ],[1 13 16 17  n-1 n])
+
+signalON=Y_val(:,n)*0;
+
+for i=2:13
+signalON=signalON + Y_val(:,i)*(i-1);
+end
+
+for i=14:16
+signalON=signalON + 12*(i-12);
+end
+
+for i=17:n-1
+signalON=signalON +Y_val(:,i)*440000;
+end
+
+
+signalON(end)
+ 
+signalON = (signalON - min(signalON))/(max(signalON) - min(signalON));
+
+plot(t_range, signalON)
+hold on;
+
+if (j==1)
+     load 'LFAO_DATA.txt';
+    Data=LFAO_DATA;
+    plot(Data(:,1),Data(:,2),'-*')
+    s=(Data(7,2)-Data(5,2))./(signalON(193)-signalON(145))
+% B= [t_range',signalON];
+% fileID = fopen('model1_10_Simulated_8.txt','w');
+% fprintf(fileID,'%6.2f %12.8f\n',B');
+% fclose(fileID);
+
+% A=[t_range',Y_val(:,1),Y_val(:,13),Y_val(:,16),Y_val(:,17),Y_val(:,end)];
+% fileID = fopen('oligomer_10.txt','w');
+% fprintf(fileID,'%12.12f %12.12f %12.12f %12.12f %12.12f %12.12f\n',A');
+% fclose(fileID);
+
+elseif (j==2)
+    load 'LFAO_DATA_01.txt';
+    Data=LFAO_DATA_01;
+    plot(Data(:,1),Data(:,2),'-*')
+    s=(Data(8,2)-Data(5,2))./(signalON(217)-signalON(145))
+% B= [t_range',signalON];
+% fileID = fopen('model1_1_Simulated_8.txt','w');
+% fprintf(fileID,'%6.2f %12.8f\n',B');
+% fclose(fileID);
+
+% A=[t_range',Y_val(:,1),Y_val(:,13),Y_val(:,16),Y_val(:,17),Y_val(:,end)];
+% fileID = fopen('oligomer_1.txt','w');
+% fprintf(fileID,'%12.12f %12.12f %12.12f %12.12f %12.12f %12.12f\n',A');
+% fclose(fileID);
+ else
+    load 'LFAO_DATA_00001.txt';
+   Data=LFAO_DATA_00001;
+     plot(Data(:,1),Data(:,2),'-*')
+    s=(Data(11,2)-Data(8,2))./(signalON(241)-signalON(169))
+% B= [t_range',signalON];
+% fileID = fopen('model1_01_Simulated_8.txt','w');
+% fprintf(fileID,'%6.2f %12.8f\n',B');
+% fclose(fileID);
+
+% A=[t_range',Y_val(:,1),Y_val(:,13),Y_val(:,16),Y_val(:,17),Y_val(:,end)];
+% fileID = fopen('oligomer_01.txt','w');
+% fprintf(fileID,'%12.12f %12.12f %12.12f %12.12f %12.12f %12.12f\n',A');
+% fclose(fileID);
+ end
+
+X=Data(:,2);
+Y=signalON(Data(:,1)+1);
+mdl = fitlm(Y,X)
+
+signalON (end)/signalON (250)
+
+end
+
+% B= [t_range',signalON];
+% fileID = fopen('LAFO_001_Simulated.txt','w');
+% fprintf(fileID,'%6.2f %12.8f\n',B');
+% fclose(fileID);
+
+% A=[t_range',Y_val(:,17)];
+% fileID = fopen('Fibril_50.txt','w');
+% fprintf(fileID,'%12.12f %12.12f \n',A');
+% fclose(fileID);
+
+end
